bf8ce6bf83229ad8263b54594f7812dac82795ee
Matthieu Piquerez <matthieu@MacBook-de-Matthieu-Piquerez.local>
2015-02-04 12:37:11 +0100

    Main crée l'ast.

--- main.ml
+++ main.ml
@@ -1,5 +1,8 @@
 
 open Format
+open Lexing
+open Parser
+open Ast
 
 (* Définition des options proposées pour l'exécution *)
 
@@ -23,5 +26,13 @@ let rfile =
 let () =
   try
     let c = open_in rfile in
-    exit 0
+    let lb = from_channel c in
+    try
+      let _ = file Lexer.next_token lb in
+      close_in c ;
+        exit 0
+    with SyntaxError s ->
+      let b, e = lexeme_start_p lb, lexeme_end_p lb in
+      eprintf "File \"%s\", line %d, characters %d-%d:\n%s@." rfile
+        b.pos_lnum (b.pos_cnum - b.pos_bol + 1) (e.pos_cnum - b.pos_bol + 1) s
   with e -> eprintf "Anomaly: %s\n@." (Printexc.to_string e)
