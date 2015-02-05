ae3762acbaf23fc149f1755f417fc75b2fa435d4
Matthieu Piquerez <matthieu@MacBook-de-Matthieu-Piquerez.local>
2015-02-05 16:12:23 +0100

    Suppression typ_var, Hashtbl changées en Map.

--- interp.ml
+++ interp.ml
@@ -5,58 +5,86 @@ open Ast
 
 exception Error of loc * string
 
-type var_typ =
-  | Tarray of int * var_typ
-  | Tint
-
 type var =
   | Vint of int ref
-  | Varray of tvar array (* TODO très lourd *)
+  | Varray of var array
 
-and tvar = { typ : var_typ; var : var }
+type tfun = { typ : typ; args : (loc_ident * typ) list; body : stat }
 
-type tfun = { ftyp : typ; args : (loc_ident * typ) list; body : stat }
+module Mstr = Map.Make(String)
 
-module Mvar = Map.Make(String)
+(* Interprétation... *)
 
-let funs = Hashtbl.create 13
-let vars = Hashtbl.create 53
+exception Return of var
+exception ReturnVoid
+
+let rec newVar =
+  assert false
+
+let rec interpExpr =
+  assert false
+
+and interpStat vars = function
+  | Sexpr e -> let _ = interpExpr vars e in vars
+  | Sdo ls -> List.fold_right (fun vars s -> interpStat s vars) ls vars ; vars
+  | Sreturn e -> raise (Return (interpExpr vars e))
+  | SreturnVoid -> raise ReturnVoid
+  | Sif (e, s) -> ( let cdt = interpExpr vars e in
+    match cdt with
+      | Vint n -> if !n = 0 then
+          ()
+        else
+          interpStat vars s
+      | _ -> assert false ) ;
+    vars
+  | Sifelse (e, s1, s2) -> ( let cdt = interpExpr vars e in
+    match cdt with
+      | Vint n -> if !n = 0 then
+          interpStat vars s2
+        else
+          interpStat vars s1
+      | _ -> assert false ) ;
+    vars
+  | Swhile (e, s) as w -> interpStat vars (Sif (e, Sdo [s ; w]))
+  | Sdecl (lid, t) -> assert false (* List.fold_right newVar t lid vars *)
 
 (** Lecture du fichier **)
 
 let rec newGlobalVar = function
   | VTarray ({ expr = Econst (Cint n) }, t) when n > 0 ->
-      let arr = Array.init n (fun _ -> newGlobalVar t) in
-      { typ = Tarray (n, arr.(0).typ); var = Varray arr }
-  | TTint -> { typ = Tint; var = Vint (ref 0) }
+      Varray (Array.init n (fun _ -> newGlobalVar t))
+  | TTint -> Vint (ref 0)
   | VTarray _ -> assert false
   | _ -> assert false
 
-let addGlobalVar t id =
-  if Hashtbl.mem vars id.ident then
+let addGlobalVar t id vars =
+  if Mstr.mem id.ident vars then
     assert false ;
-  Hashtbl.add vars id.ident (newGlobalVar t)
+  Mstr.add id.ident (newGlobalVar t) vars
 
-let addFun t id args body =
-  if Hashtbl.mem funs id.ident then
+let addFun t id args body funs =
+  if Mstr.mem id.ident funs then
     assert false ;
   match t with
     | VTarray _ -> assert false
-    | _ -> Hashtbl.add funs id.ident { ftyp = t; args = args; body = body }
+    | _ -> Mstr.add id.ident { typ = t; args = args; body = body } funs
   
 let addStaticVar t id statics =
-  if Mvar.mem id.ident statics then
+  if Mstr.mem id.ident statics then
     statics
   else
-    Mvar.add id.ident (newGlobalVar t) statics
+    Mstr.add id.ident (newGlobalVar t) statics
 
 
 let interpFile ldecl statics =
-  let rec aux = function    (* Ast.file -> Mvar *)
-    | [] -> statics
-    | Dident (lid, t)::q -> List.iter (addGlobalVar t) lid ; aux q
-    | DstaticIdent (lid, t)::q -> List.iter (addGlobalVar t) lid ;
-      List.fold_right (addStaticVar t) lid (aux q)
-    | Dfun (id, t, args, body)::q -> addFun t id args body ; aux q in
+  let rec aux statics vars funs = function    (* Ast.file -> Mstr *)
+    | [] -> (statics, vars, funs)
+    | Dident (lid, t)::q ->
+      aux statics (List.fold_right (addGlobalVar t) lid vars) funs q
+    | DstaticIdent (lid, t)::q ->
+      aux (List.fold_right (addStaticVar t) lid statics)
+        (List.fold_right (addGlobalVar t) lid vars) funs q
+    | Dfun (id, t, args, body)::q ->
+        aux statics vars (addFun t id args body funs) q in
   let statics = aux ldecl in
   assert false
