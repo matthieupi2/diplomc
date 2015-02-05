385e8db57edbff87fa1446cdb4c44dbfc2aef7af
Matthieu Piquerez <matthieu@MacBook-de-Matthieu-Piquerez.local>
2015-02-05 18:15:18 +0100

    Ajout début interpExpr.

--- interp.ml
+++ interp.ml
@@ -8,6 +8,7 @@ exception Error of loc * string
 type var =
   | Vint of int ref
   | Varray of var array
+  | Vvoid                   (* TODO ... *)
 
 type tfun = { typ : typ; args : (loc_ident * typ) list; body : stat }
 
@@ -21,6 +22,16 @@ let add_list f t lid map =
 exception Return of var
 exception ReturnVoid
 
+let rec areSameTypes a b = match a, b with
+  | Vint _, Vint _ -> true
+  | Varray a, Varray b -> areSameTypes a.(0) b.(0)
+  | _ -> false
+
+let rec isOfType typ a = match a, typ with
+  | Vint _, TTint -> true
+  | Varray a, FTarray typ -> isOfType typ a.(0)
+  | _ -> false
+
 let rec newVar vars funs = function
   | VTarray (e, t) -> ( match interpExpr vars funs e with
     | Vint i when !i > 0 -> Varray (Array.init !i (fun _ -> newVar vars funs t))
@@ -28,8 +39,50 @@ let rec newVar vars funs = function
   | TTint -> Vint (ref 0)
   | _ -> assert false
 
-and interpExpr _ _ _ =
+and interpLeft _ _ _ =
   assert false
+
+(* TODO ajouter \0 à la fin des chaînes de caractères *)
+(* TODO assuré 32 bit *)
+
+and interpExpr vars funs e = match e.expr with
+  | Eleft l -> interpLeft vars funs l
+  | Eassign (l, e) -> let l = interpLeft vars funs l in
+    let e = interpExpr vars funs e in
+    if areSameTypes l e then
+      match l, e with
+        | Vint refl, Vint v -> refl := !v ; l
+        | Varray arrl, Varray arr ->
+          for i = 0 to min (Array.length arrl) (Array.length arr) - 1 do
+            arrl.(i) <- arr.(i)
+          done ;
+          l
+        | _ -> assert false
+    else
+      assert false
+  | Ecall (id, args) -> (
+    try
+      let f = Mstr.find id.ident funs in
+      let rec add_args vars formals args = match formals, args with
+        | [], [] -> vars
+        | formal::qf, arg::qa -> 
+          let v = interpExpr vars funs arg in
+          if isOfType (snd formal) v then
+            add_args (Mstr.add (fst formal).ident v vars) qf qa
+          else
+            assert false
+        | _ -> assert false in
+      try
+        ignore (interpStat (add_args vars f.args args) funs f.body) ;
+        if f.typ = FTvoid then
+          raise ReturnVoid
+        else
+          assert false
+      with
+        | Return v -> v
+        | ReturnVoid -> Vvoid
+    with Not_found -> assert false )
+  | _ -> assert false
 
 and interpStat vars funs = function
   | Sexpr e -> let _ = interpExpr vars funs e in vars
